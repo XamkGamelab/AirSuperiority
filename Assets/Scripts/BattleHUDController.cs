9d1ef7ddada90334341a34effda8f938355304f5
Jenna Mäki <fjema016@edu.xamk.fi>
2025-03-19 21:20:10 +0200

    UI canvas and Battle HUD assets
    Setting up Battle HUD for UI canvas.

--- Assets/Scripts/BattleHUDController.cs
+++ Assets/Scripts/BattleHUDController.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
+using static StatsManager;
 
 public class BattleHUDController : MonoBehaviour
 {
-
+    // UI elements for stats
+    public Text scoreText;
+    public Slider healthSlider;
+    public Slider shieldSlider;
+    public Text ammoText;
+    public Text timeText;
+    
     private bool updatingHud = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // UpdateHUD();
+        // Calling for UpdateHUD() function
     }
 
     // Update is called once per frame
@@ -25,6 +34,17 @@ public class BattleHUDController : MonoBehaviour
             updatingHud = false;
         }
     }
+
+    //  private void UpdateHUD()
+    //   {
+    //      PlayerData playerData = StatsManager.Instance.player[0];
+    //      scoreText.text = "Score: " + playerData.Score.ToString();
+    //      healthSlider.value = playerData.Health / 100f;
+    //      shieldSlider.value = playerData.Shield / 100f;
+    //      ammoText.text = "Ammo: " + playerData.CurrentGun.AmmoCount.ToString();
+    //      timeText.text = "Time: " + StatsManager.Instance.GetPlayTime().ToString("F2") + "s";
+    //   }
+
 
     //Here will be coroutine for Updating Hud
 }
