fee7495bfeed7e972897f42b62c55bb41d04a788
Antti <a.no.pain@gmail.com>
2025-03-20 14:54:16 +0200

    still fixing spawnManager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -30,7 +30,7 @@ public class SpawnManager : MonoBehaviour
 {
     public static SpawnManager Instance { get; private set; }
 
-    public string tilemapObjectName = "PlayArea;";          //Tilemap area where spawning is possible
+    public string tilemapObjectName = "PlayArea";          //Tilemap area where spawning is possible
     public LayerMask LevelElementLayer;                     //LayerMask for LevelElement collision detection
     public string resourcesFolder1 = "Prefabs/SpawnItems";  //Resources folder for spawnable items
     public string resourcesFolder2 = "Prefabs/Guns";        //Resources folder for spawnable guns
@@ -107,6 +107,7 @@ public class SpawnManager : MonoBehaviour
     private void FindPlayAreaBounds()                                   //Determine tilemap boundaries
     {
         GameObject tilemapParent = GameObject.Find("Tilemap");  //Search tilemap by layerName
+        Debug.Log($"Found GameObject {tilemapParent.name}");
 
         if (tilemapParent)
         {
@@ -117,6 +118,7 @@ public class SpawnManager : MonoBehaviour
                 {
                     playAreaTilemap = tilemap;
                     spawnBounds = playAreaTilemap.cellBounds;
+                    Debug.Log($"Found bound values x: {spawnBounds.x} y: {spawnBounds.y}");
                     return;
                 }
             }
@@ -132,7 +134,9 @@ public class SpawnManager : MonoBehaviour
     private void CollectValidSpawnPositions()                           //Determine valid spawnPoints
     {
         validSpawnPositions.Clear();
-
+        Debug.Log($"Valid SpawnPositions Cleared: {validSpawnPositions.Count}");
+        string temp = LevelElementLayer.ToString();
+        Debug.Log($"Using LayerMask {LevelElementLayer.value} for detecting overlap.");
         foreach (Vector3Int cellPosition in spawnBounds.allPositionsWithin)
         {
             if (playAreaTilemap.HasTile(cellPosition))                  //Check if the tile exist in the playArea
@@ -144,7 +148,7 @@ public class SpawnManager : MonoBehaviour
                 }
             }
         }
-
+        Debug.Log($"Valid SpawnPositions: {validSpawnPositions.Count}");
         if (validSpawnPositions.Count == 0)
         {
             Debug.LogWarning("No valid spawn positions found!");
