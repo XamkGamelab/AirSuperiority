81be80575b2813fa0a000baffc39bedbc0204279
Antti <a.no.pain@gmail.com>
2025-04-11 09:12:52 +0300

    Fixed Time calculation #79

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class AudioController : MonoBehaviour
 {
     public static AudioController Instance { get; private set; }
-
     private void Awake()
     {
         if (Instance != null && Instance != this)
